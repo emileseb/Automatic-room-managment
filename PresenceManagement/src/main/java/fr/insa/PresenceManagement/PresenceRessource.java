bd19786ca0e625d48524823b9749c5dafad8f445
Etienne <luxumbra27@zoho.com>
2020-11-27 17:47:22 +0100

    Restructure presence service. Add JSON functionality.

--- PresenceManagement/src/main/java/fr/insa/PresenceManagement/PresenceRessource.java
+++ PresenceManagement/src/main/java/fr/insa/PresenceManagement/PresenceRessource.java
@@ -2,12 +2,22 @@ package fr.insa.PresenceManagement;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @RestController
 public class PresenceRessource {
 
+    private boolean presence = true;
+
     @GetMapping("/presence")
     public String presenceStatus() {
-        return "{\"presence\": true}";
+        JSONObject resp = new JSONObject();
+        try {
+            resp.put("presence", this.presence);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return resp.toString();
     }
 }
